3d70dd6daba39b08b97be1131b7b388f6a051773
Javier Ortiz-Tudela <fjavierot@gmail.com>
2022-05-11 13:16:57 +0200

    Update required_packages.R

--- required_packages.R
+++ required_packages.R
@@ -1,2 +1,3 @@
-# This next line will install all the packages needed for the workshop. You just need to run this script once.
-install.packages(c("PCKGNAME", "PCKGNAME", "PCKGNAME", "PCKGNAME"))
+# The following lines will install all the packages needed for the workshop. You just need to run this script once.
+cmb_packages <- c("PCKGNAME", "PCKGNAME", "PCKGNAME", "PCKGNAME")
+install.packages(cmb_packages)
